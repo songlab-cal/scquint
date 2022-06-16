5856644177f60043667135830da848f2bf9b81fd
Gonzalo Benegas <gbenegas@berkeley.edu>
2022-06-16 09:55:04 -0700

    Add warning about restarting runtime in Google Colab

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 
 setup(
     name='scquint',
-    version='0.3.1',
+    version='0.3.2',
     description='scQuint',
     url='http://github.com/songlab-cal/scquint',
     author='Gonzalo Benegas',
